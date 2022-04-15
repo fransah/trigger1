3f420ca122a28b58655c542e119348badc3c8a6b
Frans Wessels <frans.wessels@ah.nl>
2022-04-15 10:00:23 +0200

    adding only component test adding simple unit test

---
+++ src/test/kotlin/com/ahold/triggers/trigger1/SimpleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.ahold.triggers.trigger1
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SimpleUnitTest {
+
+    @Test
+    fun testSomethingStupid(){
+        assertEquals(1+1, 2)
+    }
+}
